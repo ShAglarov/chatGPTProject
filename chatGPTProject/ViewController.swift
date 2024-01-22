ab0d4e04f06c0d5a5abc3af5573fc67fa2f62d34
ShAglarov <shamil.aglarov@gmail.com>
2024-01-22 21:42:36 +0300

    Initial Commit

---
+++ chatGPTProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatGPTProject
+//
+//  Created by Shamil Aglarov on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
