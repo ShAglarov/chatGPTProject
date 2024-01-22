039e5af2a34296d29e4c271b68917c9aa6a8ea9f
ShAglarov <shamil.aglarov@gmail.com>
2024-01-22 22:09:12 +0300

    step-2

--- chatGPTProject/ViewController.swift
+++ chatGPTProject/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Создаем экземпляр UITableView
+    private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Инициализируем и настраиваем tableView
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        // Добавляем tableView как subview
+        view.addSubview(tableView)
+        
+        // Настройка Auto Layout
+        setupTableViewConstraints()
     }
 
+    // MARK: - UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Верните количество строк в вашем разделе
+        return 10 // Примерное значение
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Создаем и возвращаем ячейку
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate Methods
+    // Здесь можно добавить методы, относящиеся к делегату UITableView
+
+    // MARK: - Auto Layout Setup
+    private func setupTableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+    }
 }
 
